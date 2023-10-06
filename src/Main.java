4f58b2fc8db3a5bab18750dd4b9111ba0417d495
Audrey Tan <audreyt2026@student.cis.edu.hk>
2023-10-06 09:52:11 +0800

    finish code

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,32 @@ public class Main {
 
     public static void main(String[] args) {
 
+        int resultOne = add(4, 4);
+        System.out.println(resultOne);
+
+        String resultTwo = greeting("Bob");
+        System.out.println(resultTwo);
+
+        String toBePrinted = "Hello, world!";
+        printMe(toBePrinted);
+
     }
 
+
+    public static int add (int numOne, int numTwo)
+    {
+              return numOne + numTwo;
+
+
+    }
+
+    public static String greeting (String name) {
+        String s = "Bonjour," + name + "!";
+        return s;
+
+    }
+
+            public static void printMe(String toBePrinted) {
+            System.out.println(toBePrinted);
+        }
 }
